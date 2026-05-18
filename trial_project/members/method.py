52df1f7f4680e91007c2e64f8053dc6b90644ba8
anaspvt77-max <anaspvt77@gmail.com>
2026-05-18 14:38:08 +0530

    added inherittence& C:\Users\User\AppData\Local\Programs\Python\Python312\python.exe c:/Users/User/Desktop/internsh.p/trial_project/members/inherittence.py

--- trial_project/members/method.py
+++ trial_project/members/method.py
@@ -10,13 +10,29 @@
       
 
 
-class person:
-    def __init__(self, name):
-        self.name = name
+# class person:
+#     def __init__(self, name):
+#         self.name = name
 
-    def greet(self):
-        print("Hello , my name is " + self.name)
+#     def greet(self):
+#         print("Hello , my name is " + self.name)
 
-p2 = person("Adhnan my.")
-p2.greet()
+# p2 = person("Adhnan my.")
+# p2.greet()
       
+class person:
+    def __init__(self, fname, lname):
+       self.firstname = fname
+       self.lastname = lname
+
+    def printname(self):
+       print(self.firstname, self.lastname)
+
+x = person("john", "doe")
+x.printname()
+
+class student(person):
+   pass
+
+x = student("mike", "olsen")
+x.printname()
